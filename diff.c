ebea27e8d9424fa6ccfa648aab9a688822ba4516
Your Name <you@example.com>
2019-10-20 12:48:28 +0530

    program for subtraction of two numbers

---
+++ diff.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int x,y,diff=0;
+    printf("enter two numbers";
+    scanf("%d%d",&x,&y);
+    diff=x-y;
+    printf("difference of two numbers is %d",diff);
+
+    )
+}
