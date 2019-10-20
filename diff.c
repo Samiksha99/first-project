bc343fdb8869d58b9ab05df69793a1125628404e
Your Name <you@example.com>
2019-10-20 12:58:48 +0530

    Add program for subtraction

---
+++ diff.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int x,y,diff=0;
+    printf("enter two numbers";
+    scanf("%d%d",&x,&y);
+    diff=x-y;
+    printf("difference of two numbers is %d",diff);
+}
